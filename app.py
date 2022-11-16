67cadf9d36804210e661adc90aaf9d06ac6dd492
MJ Sung <themrsung@gmail.com>
2022-11-16 23:30:21 +0900

    sentence cap

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ def home():
 @app.route("/til", methods=["POST"])
 def get_TIL():
     sentences_receive = request.form["sentences_give"]
+    if int(sentences_receive) > 10000:
+        return jsonify({"TIL": "10,000문장 초과하시면 안됩니다!!!"})
     result = til_generator.run(int(sentences_receive), True)
     return jsonify({"TIL" : result})
 
