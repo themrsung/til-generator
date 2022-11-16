e4fb218dfa7f1edb85019739cf85168b81c5a19c
MJ Sung <themrsung@gmail.com>
2022-11-16 10:41:11 +0900

    run.cmd changed

--- run.cmd
+++ run.cmd
@@ -1,38 +1,9 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFA9cSQmQAES0A5EO4f7+02FLesH5qN4cd4vS1LOyFMk/qmLhe5M/125Vivc4JTdnWTeFSz0VnUFw+GGdMqc=
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 echo off
 python -m pip install pyjosa
+python -m pip install flask
+python -m pip install request
+python -m pip install render_template
+python -m pip install jsonify
 color 0a
 cls
 start cmd.exe @cmd /k "python web.py"
